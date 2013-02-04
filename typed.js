bd07776c538f62c3942b942777e6baf175cb5535
Sebastian Wiedenroth <wiedi@frubar.net>
2013-02-04 02:32:48 +0100

    typedmatrix: getters for rows and columns

--- typed.js
+++ typed.js
@@ -25,6 +25,23 @@ TypedMatrix.prototype.setAll = function(value) {
 	}
 }
 
+TypedMatrix.prototype.getRow = function(row) {
+	/* this should be better with .slice - but node gives us just a new reference */
+	var v = new this.type(this.cols)
+	for(var i = 0; i < this.cols; i++) {
+		v[i] = this.data[this.rows * row + i]
+	}
+	return v
+}
+
+TypedMatrix.prototype.getColumn = function(col) {
+	var v = new this.type(this.rows)
+	for(var i = 0; i < this.rows; i++) {
+		v[i] = this.data[this.rows * i + col]
+	}
+	return v
+}
+
 TypedMatrix.prototype.rowScalarProduct = function(row, matrix2, row2) {
 	if(row > this.rows || row2 > matrix2.rows || this.cols != matrix2.cols) {
 		throw new Error("invalid dimension")
@@ -34,8 +51,10 @@ TypedMatrix.prototype.rowScalarProduct = function(row, matrix2, row2) {
 	var offset1 = row  * this.rows
 	var offset2 = row2 * matrix2.rows
 	for(var i = 0; i < this.cols; i++) {
-		result += this.data[offset1 + c] * matrix2.data[offset2 + c]
+		result += this.data[offset1 + i] * matrix2.data[offset2 + i]
 	}
 	return result
 }
+
+module.exports = TypedMatrix
 
